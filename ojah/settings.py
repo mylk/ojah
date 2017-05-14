71bc8df2e41f82cf11918d1ba8c678b613d22f73
Kostas Milonas <milonas.ko@gmail.com>
2017-05-14 12:53:12 +0300

    adds rendering of rss feed

--- ojah/settings.py
+++ ojah/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+RSS_FEED_TITLE = 'Ojah. the positive news aggregator'
+RSS_FEED_DESCRIPTION = 'Most positive news of the internet in one feed!'
