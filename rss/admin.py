053aca1f64a91fe3148b82c8e82d02b3202a9e7d
Kostas Milonas <milonas.ko@gmail.com>
2018-07-24 23:17:17 +0300

    modify the admin page of news items

--- rss/admin.py
+++ rss/admin.py
@@ -3,6 +3,14 @@ from .models.source import Source
 from .models.newsitem import NewsItem
 from .models.corpus import Corpus
 
+
+class NewsItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source', 'score')
+    list_filter = ['score']
+    search_fields = ['title']
+    ordering = ['-added_at']
+
+
 admin.site.register(Source)
-admin.site.register(NewsItem)
+admin.site.register(NewsItem, NewsItemAdmin)
 admin.site.register(Corpus)
