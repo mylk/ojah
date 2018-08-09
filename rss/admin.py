d8b9d7d222ebb002a4297fb87e2ce837723443bb
Kostas Milonas <milonas.ko@gmail.com>
2018-08-09 18:05:30 +0300

    fields and filters addition to admin interface

--- rss/admin.py
+++ rss/admin.py
@@ -4,13 +4,18 @@ from .models.newsitem import NewsItem
 from .models.corpus import Corpus
 
 
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_crawl')
+    ordering = ['-last_crawl']
+
+
 class NewsItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'source', 'score', 'published')
-    list_filter = ['score']
+    list_display = ('title', 'source', 'score', 'published', 'added_at')
+    list_filter = ['score', 'source']
     search_fields = ['title']
     ordering = ['-added_at']
 
 
-admin.site.register(Source)
+admin.site.register(Source, SourceAdmin)
 admin.site.register(NewsItem, NewsItemAdmin)
 admin.site.register(Corpus)
