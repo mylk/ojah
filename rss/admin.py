60363ff892793933af6f2e234f45a9425fccc8de
Kostas Milonas <milonas.ko@gmail.com>
2018-09-15 23:45:56 +0300

    adds publish and unpublish actions on news item admin list form

--- rss/admin.py
+++ rss/admin.py
@@ -4,6 +4,21 @@ from .models.newsitem import NewsItem
 from .models.corpus import Corpus
 
 
+def news_item_publish(model_admin, request, query_set):
+    for news_item in query_set:
+        news_item.published = True
+        news_item.save()
+
+news_item_publish.short_description = 'Publish'
+
+def news_item_unpublish(model_admin, request, query_set):
+    for news_item in query_set:
+        news_item.published = False
+        news_item.save()
+
+news_item_unpublish.short_description = 'Unpublish'
+
+
 class SourceAdmin(admin.ModelAdmin):
     list_display = ('name', 'last_crawl')
     ordering = ['-last_crawl']
@@ -14,6 +29,7 @@ class NewsItemAdmin(admin.ModelAdmin):
     list_filter = ['score', 'published', 'source']
     search_fields = ['title']
     ordering = ['-added_at']
+    actions = [news_item_publish, news_item_unpublish]
 
 
 admin.site.register(Source, SourceAdmin)
