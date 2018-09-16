73f74fd6abff5035211088b669b847e23bd38ee0
Kostas Milonas <milonas.ko@gmail.com>
2018-09-16 15:23:19 +0300

    adds corpus related actions on news item admin list form

--- rss/admin.py
+++ rss/admin.py
@@ -8,15 +8,34 @@ def news_item_publish(model_admin, request, query_set):
     for news_item in query_set:
         news_item.published = True
         news_item.save()
-
 news_item_publish.short_description = 'Publish'
 
 def news_item_unpublish(model_admin, request, query_set):
     for news_item in query_set:
         news_item.published = False
         news_item.save()
-
 news_item_unpublish.short_description = 'Unpublish'
+
+def news_item_corpus_create_positive(model_admin, request, query_set):
+    for news_item in query_set:
+        corpus = Corpus()
+        corpus.news_item = news_item
+        corpus.positive = True
+        corpus.save()
+news_item_corpus_create_positive.short_description = 'Corpus - Create positive'
+
+def news_item_corpus_create_negative(model_admin, request, query_set):
+    for news_item in query_set:
+        corpus = Corpus()
+        corpus.news_item = news_item
+        corpus.positive = False
+        corpus.save()
+news_item_corpus_create_negative.short_description = 'Corpus - Create negative'
+
+def news_item_publish_and_corpus_create_positive(model_admin, request, query_set):
+    news_item_corpus_create_positive(model_admin, request, query_set)
+    news_item_publish(model_admin, request, query_set)
+news_item_publish_and_corpus_create_positive.short_description = 'Publish and create positive Corpus'
 
 
 class SourceAdmin(admin.ModelAdmin):
@@ -29,7 +48,13 @@ class NewsItemAdmin(admin.ModelAdmin):
     list_filter = ['score', 'published', 'source']
     search_fields = ['title']
     ordering = ['-added_at']
-    actions = [news_item_publish, news_item_unpublish]
+    actions = [
+        news_item_publish,
+        news_item_unpublish,
+        news_item_corpus_create_positive,
+        news_item_corpus_create_negative,
+        news_item_publish_and_corpus_create_positive
+    ]
 
 
 admin.site.register(Source, SourceAdmin)
