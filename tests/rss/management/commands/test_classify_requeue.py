80dbe85f15007545a268b2f1f9ddee1fd1bce95e
Kostas Milonas <milonas.ko@gmail.com>
2018-11-27 16:05:51 +0200

    adds tests for requeue command

---
+++ tests/rss/management/commands/test_classify_requeue.py
@@ -0,0 +1,60 @@
+from django.conf import settings
+from django.contrib.admin.sites import AdminSite
+from django.test import TestCase
+import logging
+import mock
+import pika
+from rss.management.commands import classify_requeue
+from rss.models.newsitem import NewsItem
+
+
+class CommandTestCase(TestCase):
+
+    def setUp(self):
+        # retain the original imported packages
+        classify_requeue.pika_real = classify_requeue.pika
+        classify_requeue.logging_real = classify_requeue.logging
+
+        # mock pika.BlockingConnection
+        self.connection = mock.MagicMock()
+        classify_requeue.pika.BlockingConnection = mock.MagicMock(return_value=self.connection)
+        # mock pika.BlockingConnection.channel
+        self.channel = mock.MagicMock()
+        self.connection.channel = mock.MagicMock(return_value=self.channel)
+        # mock ConnectionParameters
+        classify_requeue.pika.ConnectionParameters = mock.MagicMock()
+
+        # mock method used by logging
+        self.logger = mock.MagicMock()
+        classify_requeue.logging.getLogger = mock.MagicMock(return_value=self.logger)
+
+        self.command = classify_requeue.Command()
+
+    def tearDown(self):
+        # revert the packages to the original imported
+        classify_requeue.pika = classify_requeue.pika_real
+        classify_requeue.logging = classify_requeue.logging_real
+
+    def test_handle_exits_when_no_newsitems(self):
+        # the method being tested
+        self.command.handle()
+
+        classify_requeue.pika.BlockingConnection.assert_not_called()
+        self.logger.infoassert_called_once_with('All news items are already classified!')
+
+    def test_handle_publishes_when_newsitems_exist(self):
+        news_item = NewsItem()
+        news_item.title = 'foo'
+        news_item.save()
+
+        # the method being tested
+        self.command.handle()
+
+        classify_requeue.pika.BlockingConnection.assert_called_once()
+        classify_requeue.pika.ConnectionParameters.assert_called_once()
+        self.connection.channel.assert_called_once()
+        self.channel.queue_declare.assert_called_once_with(queue=settings.QUEUE_NAME_CLASSIFY, durable=True)
+        self.channel.basic_publish.assert_called_once()
+
+        self.logger.info.assert_any_call('Found 1 news items that need to be classified.')
+        self.logger.info.assert_any_call('Successfully re-queued #1 "foo"!')
