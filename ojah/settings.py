117f8565b7ae5b92eed4f132e16eb81c63a43afb
Kostas Milonas <milonas.ko@gmail.com>
2020-03-27 20:23:16 +0200

    bumps django to fix possible vulnerabilities (CVE-2020-7471, CVE-2019-19844)

--- ojah/settings.py
+++ ojah/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'web/static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'web/staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Logging
 
