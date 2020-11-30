d4659d970b0aafb258e9554be3b350824690088a
Kostas Milonas <milonas.ko@gmail.com>
2020-12-01 00:39:54 +0200

    adds admin actions for activation of corpora

--- web/management/admin/corpus.py
+++ web/management/admin/corpus.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
 
 
+def corpus_activate(model_admin, request, query_set):
+    for corpus in query_set:
+        corpus.active = True
+        corpus.save()
+
+
+corpus_activate.short_description = 'Activate'
+
+
+def corpus_deactivate(model_admin, request, query_set):
+    for corpus in query_set:
+        corpus.active = False
+        corpus.save()
+
+
+corpus_deactivate.short_description = 'Deactivate'
+
+
 class CorpusAdmin(admin.ModelAdmin):
 
     @staticmethod
     def get_news_item_title(obj):
         return obj.news_item.title
 
-    list_display = ['get_news_item_title', 'positive', 'added_at']
-    list_filter = ['positive']
+    list_display = ['get_news_item_title', 'positive', 'active', 'added_at']
+    list_filter = ['positive', 'active']
     ordering = ['-added_at']
+    actions = [
+        corpus_activate,
+        corpus_deactivate,
+    ]
